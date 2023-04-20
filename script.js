6d3221f02415c456866ff77749659c0b2a83bb08
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-04-20 21:46:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-//your code here
+document.getElementById('addTodoBtn').addEventListener('click', function() {
+	let todo = document.getElementById('newTodoInput').value;
+	const li = document.createElement('li');
 
+	if(todo == ""){
+		return null;
+	}else {
+		li.innerText = todo;
+	}
+
+	document.getElementById('toDoList').append(li);
+})
